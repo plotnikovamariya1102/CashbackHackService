3162e1d3c5df738392725193805920b11a15ca87
Plotnikova Mariya <m.buryakova@yandex.ru>
2021-03-23 11:02:54 +0500

    Added tests and dependency JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,13 +1,10 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
-
-    @Test
+    @org.junit.Test
     public void showBelowBoundary(){
         int purchaseAmount = 500;
         int actualResult = service.remain(purchaseAmount);
@@ -15,14 +12,14 @@ public class CashbackHackServiceTest {
         assertEquals(actualResult, expectedResult);
 
     }
-    @Test
+    @org.junit.Test
     public void showHigherBoundary(){
         int purchaseAmount = 1500;
         int actualResult = service.remain(purchaseAmount);
         int expectedResult = 500;
         assertEquals(actualResult, expectedResult);
 
-    }@Test
+    }@org.junit.Test
     public void showNullBoundary(){
         int purchaseAmount = 0;
         int actualResult = service.remain(purchaseAmount);
@@ -30,12 +27,13 @@ public class CashbackHackServiceTest {
         assertEquals(actualResult, expectedResult);
 
     }
-    @Test
+    @org.junit.Test
     public void showEqualBoundary(){
         int purchaseAmount = 1000;
         int actualResult = service.remain(purchaseAmount);
-        int expectedResult = 1000;
+        int expectedResult = 0;
         assertEquals(actualResult, expectedResult);
 
     }
+
 }
